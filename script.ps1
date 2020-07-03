6bd19b9e3beb76246bf358b71fa9211462a79245
ajayvamsisree <ajayvamsisree@gmail.com>
2020-07-03 12:11:47 +0530

    updating

--- script.ps1
+++ script.ps1
@@ -1,10 +1,11 @@
 git clone "https://github.com/ajayvamsisree/test.git"
 cd test
 git status
-echo "hello 324" >>abc.txt
+echo "heljasdhajdhajljl" >>abc.txt
 git status
 git add .
 git commit -m "add"
 git config --global user.name "ajayvamsisree"
 git config --global user.email "ajayvamsisree@gmail.com"
-
+git push -u origin master
+cd ..
