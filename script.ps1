a5aca097a1043e0da0092ea0d53ce2e7c3e8eb20
ajayvamsisree <ajayvamsisree@gmail.com>
2020-07-03 13:14:47 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -5,7 +5,7 @@ git status
 echo "heljasdhajdhahjghjghfgfgh kjhj jljl" >>abc.txt
 git status
 git add abc.txt
-git commit -m "add"
+git commit . -m "add"
 git config --global user.name "ajayvamsisree"
 git config --global user.email "ajayvamsisree@gmail.com"
 git push -u origin master
